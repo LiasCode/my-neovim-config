96511bf20328f7b0b93639731221914d540d4b46
LiasCode <liasraul00@gmail.com>
2023-04-06 01:28:11 -0400

    add toggle terminal plugin

--- lua/liascode/packer.lua
+++ lua/liascode/packer.lua
@@ -8,6 +8,14 @@ return require('packer').startup(function(use)
   use('wbthomason/packer.nvim')
 
   use {
+    "akinsho/toggleterm.nvim",
+    tag = '*',
+    config = function()
+      require("toggleterm").setup()
+    end
+  }
+
+  use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     -- or                            , branch = '0.1.x',
     requires = {
