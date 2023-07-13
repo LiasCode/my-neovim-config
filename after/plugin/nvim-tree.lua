76e0828f6655da3fa625815236e09f31ebe27489
LiasCode <liasraul00@gmail.com>
2023-07-13 13:43:49 -0400

    disable nvim-tree for a while

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,22 +1,22 @@
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrw = 1
-
-require("nvim-tree").setup({
-  sort_by = "type",
-  view = {
-    width = 30,
-  },
-  disable_netrw = true,
-  renderer = {
-    group_empty = false,
-    add_trailing = true,
-  },
-  filters = {
-    dotfiles = false,
-    -- custom = { "^.git$", "^node_modules$" }
-  },
-  git = {
-    enable = true,
-    ignore = false,
-  },
-});
+-- vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+--
+-- require("nvim-tree").setup({
+--   sort_by = "type",
+--   view = {
+--     width = 30,
+--   },
+--   disable_netrw = true,
+--   renderer = {
+--     group_empty = false,
+--     add_trailing = true,
+--   },
+--   filters = {
+--     dotfiles = false,
+--     -- custom = { "^.git$", "^node_modules$" }
+--   },
+--   git = {
+--     enable = true,
+--     ignore = false,
+--   },
+-- });
--- lua/liascode/remap.lua
+++ lua/liascode/remap.lua
@@ -9,8 +9,8 @@ vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
--- vim.keymap.set("n", "<leader>ef", "<Esc>:Ex .<CR>");
-vim.keymap.set("n", "<leader>ef", ":NvimTreeToggle<CR>");
+vim.keymap.set("n", "<leader>ef", "<Esc>:Ex .<CR>");
+-- vim.keymap.set("n", "<leader>ef", ":NvimTreeToggle<CR>");
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
