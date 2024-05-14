5fb5f9f837d967517ee8b7211404ba9c6c42852f
LiasCode <liasraul00@gmail.com>
2024-05-14 02:57:50 -0400

    fix: restore grep find with telescope

--- lua/liascode/plugins/telescope.lua
+++ lua/liascode/plugins/telescope.lua
@@ -14,6 +14,22 @@ return {
 
     vim.keymap.set('n', '<leader>pp', builtin.git_files, {})
 
+    vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
+
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+
     vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
   end
 }
