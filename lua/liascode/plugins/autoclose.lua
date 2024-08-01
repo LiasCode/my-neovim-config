9addf6c52840fa8bd6063ffeb6f2f3d80fa6fcda
LiasCode <liasraul00@gmail.com>
2024-07-31 23:03:50 -0400

    fix: add codeium

--- lua/liascode/plugins/autoclose.lua
+++ lua/liascode/plugins/autoclose.lua
@@ -1,18 +1,18 @@
 return {
   {
     "m4xshen/autoclose.nvim",
-    config = function ()
+    config = function()
       require("autoclose").setup()
     end
   },
   {
     "windwp/nvim-ts-autotag",
-    config = function ()
+    config = function()
       require('nvim-ts-autotag').setup({
         opts = {
           -- Defaults
-          enable_close = true, -- Auto close tags
-          enable_rename = true, -- Auto rename pairs of tags
+          enable_close = true,          -- Auto close tags
+          enable_rename = true,         -- Auto rename pairs of tags
           enable_close_on_slash = false -- Auto close on trailing </
         },
       })
