e603774d83c23f2db79720c8205f773b3a9aaff4
LiasCode <liasraul00@gmail.com>
2024-11-18 01:23:19 -0500

    fix: find files shortcut change

--- lua/liascode/plugins/telescope.lua
+++ lua/liascode/plugins/telescope.lua
@@ -12,8 +12,8 @@ return {
 
     local builtin = require('telescope.builtin')
 
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
